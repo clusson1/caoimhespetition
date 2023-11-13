b7533554c16957926eacd2f0c6949d03dda9d75e
Caoimhe <c.lusson1@universityofgalway.ie>
2023-11-13 17:01:54 +0000

    Initial commit

---
+++ src/main/java/com/example/caoimhespetition/CaoimhespetitionApplication.java
@@ -0,0 +1,13 @@
+package com.example.caoimhespetition;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaoimhespetitionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaoimhespetitionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/caoimhespetition/CaoimhespetitionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.caoimhespetition;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaoimhespetitionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
