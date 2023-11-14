9d533fc85ce1dde4b0847bde6f81b54705ce48b6
Caoimhe <c.lusson1@universityofgalway.ie>
2023-11-14 17:24:09 +0000

    Added homepage and configured urls

--- src/main/java/com/example/caoimhespetition/CaoimhespetitionApplication.java
+++ src/main/java/com/example/caoimhespetition/CaoimhespetitionApplication.java
@@ -2,12 +2,34 @@ package com.example.caoimhespetition;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
+@Controller
 public class CaoimhespetitionApplication {
+	@RequestMapping("/")
+	public String home() {
+		return "index";
+	}
+
+	@RequestMapping("/createpetitions")
+	public String createPetitions() {
+		return "createPetitions";
+	}
+
+	@RequestMapping("/viewpetitions")
+	public String viewPetitions() {
+		return "viewPetitions";
+	}
+
+	@RequestMapping("/searchpetitions")
+	public String searchPetitions() {
+		return "searchPetitions";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(CaoimhespetitionApplication.class, args);
 	}
-
 }
+
